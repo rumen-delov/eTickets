eb037f81e4eba326a1fa0b562282f9092ac291f9
Rumen Delov <r.delov@gmx.net>
2021-12-21 08:24:40 +0100

    Add Movie model and MovieCategory enum.

---
+++ eTickets/Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1, // dafault enumeration start index is 0, here we set the start index to 1
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
