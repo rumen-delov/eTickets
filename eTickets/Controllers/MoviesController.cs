635bf72caaffd08a3ab07fda50a3ae55a9ebc9b4
Rumen Delov <r.delov@gmx.net>
2021-12-23 12:01:43 +0100

    Add MoviesController with async action.

---
+++ eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            
+            return View();
+        }
+    }
+}
