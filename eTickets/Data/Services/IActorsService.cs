dc44c7ad350b473f38357a6e18bc3db7eb89170d
Rumen Delov <r.delov@gmx.net>
2021-12-27 16:53:59 +0100

    Add folder Services to th Data and add IActorService interface.

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
