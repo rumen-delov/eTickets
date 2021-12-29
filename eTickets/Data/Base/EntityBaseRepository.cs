b2c9a272dbe6ae92605fec13d1e1b0e22acdde19
Rumen Delov <r.delov@gmx.net>
2021-12-29 10:01:20 +0100

    Define the generic GetAllAsync() and GetByIdAsync() base methods.

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        // Inject the AppDbContext
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
         public Task AddAsync(T entity)
         {
             throw new NotImplementedException();
@@ -17,14 +26,16 @@ namespace eTickets.Data.Base
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().ToListAsync(); // Generically set the type with Set<T>()
+            return result;
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+            return result;
         }
 
         public Task<T> UpdateAsync(int id, T entity)
