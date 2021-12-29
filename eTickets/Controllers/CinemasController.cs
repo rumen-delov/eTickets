b8e228dce4f339e9b9141f6e79f6d5ec1f0987b8
Rumen Delov <r.delov@gmx.net>
2021-12-29 17:46:38 +0100

    Add Create functionality to the cinemas.

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,6 +23,23 @@ namespace eTickets.Controllers
         {
             var allCinemas = await _service.GetAllAsync();            
             return View(allCinemas);
+        }
+
+        // GET: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo, Name, Description")]Cinema cinema)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
