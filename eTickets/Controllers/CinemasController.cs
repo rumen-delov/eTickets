012e5bcfb687e4ab04d021f71ce989627bf9e18e
Rumen Delov <r.delov@gmx.net>
2021-12-30 09:27:39 +0100

    Add Details functionality to the cinemas.

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -41,5 +41,16 @@ namespace eTickets.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //GET: Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) 
+            {
+                return View("NotFound");
+            }
+            return View(cinemaDetails);
+        }
     }
 }
