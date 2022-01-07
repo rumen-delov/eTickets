18a6a3acee8a80e04856564d3895e5490421ed3c
Rumen Delov <r.delov@gmx.net>
2022-01-07 17:22:26 +0100

    Set up/create a ShoppingCart class.

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            // ?? the null-calescing operator returns
+            // the value of the left-hand operand if is not null
+            // otherwise, it evaluates the right-hand operand and returns its result 
+
+            return ShoppingCartItems ??  (ShoppingCartItems = _context.ShoppingCartItems
+                
+                .Include(n => n.Movie)
+                .ToList());           
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            return _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Select(n => n.Movie.Price * n.Amount)
+                .Sum();
+        }
+    }
+}
