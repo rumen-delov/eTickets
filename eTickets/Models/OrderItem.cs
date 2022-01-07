f7027853bb470bd907749b6a8287ada71fafca43
Rumen Delov <r.delov@gmx.net>
2022-01-07 15:46:44 +0100

    Add Order and OrderItem models and their respective DbSets. Perform a migration to the DB.

---
+++ eTickets/Models/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public double Price { get; set; }
+
+        // Reference to the ordered movie
+        public int MovieId { get; set; }
+
+        [ForeignKey("MovieId")] // The ForeignKey data annotation can be omitted because
+                                // the ASP.NET MVC can understand the relationship between MovieId and Movie
+        public Movie Movie { get; set; }
+
+        // Reference to the order
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+    }
+}
