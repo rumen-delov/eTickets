936d2bde66cefac08fb696fe7bd6cab92dbddd51
Rumen Delov <r.delov@gmx.net>
2022-01-10 11:45:57 +0100

    Create IOrdersService and OrdersService and configure this service in the Startup.

---
+++ eTickets/Data/Services/IOrdersService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IOrdersService
+    {
+        // Add orders to the DB        
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress); // Authentication and autherization for the userId is
+                                                                                                    // still not implemented 
+        //  Get all orders from the DB
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
