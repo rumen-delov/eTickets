a3304b463df47811cf1a48d696d976741a846658
Rumen Delov <r.delov@gmx.net>
2022-01-10 10:48:33 +0100

    Add a ViewComponent for the number of items in the shopping cart and a Default view. Adjust the_Layout.

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        // we inject the ShoppingCart, because we are going to get the number of items in the shopping cart
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        // To call the ViewComponent we need the Invoke method
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
